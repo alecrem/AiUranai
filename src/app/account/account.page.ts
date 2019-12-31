d25f5054e16bb476818373fef4766edff559d81f
Alejandro Cremades <alecrem@users.noreply.github.com>
2019-12-31 19:30:33 +0900

    Pretending to log users in

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+const credentials = {
+  email: "karen",
+  password: "kaijo",
+}
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
+  email: string;
+  password: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async login() {
+    if(this.email == credentials.email && this.password == credentials.password) {
+      console.log("ログイン成功");
+    } else {
+      console.log("ログイン失敗");
+      console.log(this.email, this.password);
+    }
   }
 
 }
